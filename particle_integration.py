b595c180fcff6754ddd2e81f51b67c8d89e06bd4
Peter <pnolan1986@gmail.com>
2018-08-20 11:59:59 -0400

    init

---
+++ particle_integration.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 11:28:01 2018
+
+@author: pnola
+"""
+
+import numpy as np
+import scipy.integrate as sint
+import matplotlib.pyplot as plt
+from velocities import pendulum as vel_func
+from velocities import co
+plt.close('all')
+dimx = 101
+#dimy = int(np.ceil(dimx/2.5))
+dimy = int(np.ceil(dimx/2))
+t0 = 0
+tf = -5 #days
+
+#x = np.linspace(0,20000,dimx)
+#y = np.linspace(-4000,4000,dimy)
+#x = np.linspace(-10,10,dimx)
+#y = np.linspace(-10,10,dimy)
+x = np.linspace(-5.2,5.2,dimx)
+y = np.linspace(-3,3,dimy)
+dx = x[1]-x[0]
+dy = y[1]-y[0]
+yout=np.empty([len(y)*len(x),2])
+yout2=np.empty([len(y)*len(x),2])
+
+x,y = np.meshgrid(x,y)
+for k,y0 in enumerate(zip(x.ravel(),y.ravel())):
+    print(k)
+    sol = sint.solve_ivp(vel_func,[t0,tf],y0,rtol=1e-8,atol=1e-8)
+    yout[k,:] = sol.y[:,-1]
+    sol = sint.solve_ivp(vel_func,[t0,-1/24],y0,rtol=1e-8,atol=1e-8)
+    yout2[k,:] = sol.y[:,-1]
+
+fu,fv = zip(*yout)
+fu = np.reshape(fu,[dimy,dimx])
+fv = np.reshape(fv,[dimy,dimx])
+dfudy,dfudx = np.gradient(fu,dy,dx,edge_order=2)
+dfvdy,dfvdx = np.gradient(fv,dy,dx,edge_order=2)
+del fu,fv
+#JF = np.empty([dimy,dimx])
+sigma = np.empty([dimy,dimx])
+for i in range(dimy):
+    for j in range(dimx):
+        JF = np.array([[dfudx[i,j],dfudy[i,j]],[dfvdx[i,j],dfvdy[i,j]]])
+        C = np.dot(JF.T, JF)
+        lam=np.max(np.linalg.eig(C)[0])
+        if lam>=1:
+            sigma[i,j]=1.0/(2.0*abs(tf-t0))*np.log(lam)
+        else:
+            sigma[i,j]=0
+            #sigma[i,j]=1/(2.0*abs(tf-t0))*np.log(lam)
+del dfudy,dfudx,dfvdy,dfvdx
+
+fu,fv = zip(*yout2)
+fu = np.reshape(fu,[dimy,dimx])
+fv = np.reshape(fv,[dimy,dimx])
+dfudy,dfudx = np.gradient(fu,dy,dx,edge_order=2)
+dfvdy,dfvdx = np.gradient(fv,dy,dx,edge_order=2)
+del fu,fv
+#JF = np.empty([dimy,dimx])
+sigma2 = np.empty([dimy,dimx])
+for i in range(dimy):
+    for j in range(dimx):
+        JF = np.array([[dfudx[i,j],dfudy[i,j]],[dfvdx[i,j],dfvdy[i,j]]])
+        C = np.dot(JF.T, JF)
+        lam=np.max(np.linalg.eig(C)[0])
+        if lam>=1:
+            sigma2[i,j]=1.0/(2.0*abs(-1/24-t0))*np.log(lam)
+        else:
+            sigma2[i,j]=0
+            #sigma[i,j]=1/(2.0*abs(tf-t0))*np.log(lam)
+del dfudy,dfudx,dfvdy,dfvdx
+
+
+plt.figure(1)
+plt.subplot(321)
+plt.contourf(x,y,sigma2,levels=np.linspace(sigma2.min(axis=None),sigma2.max(axis=None),301))
+plt.colorbar()
+#pu,pv = vel_func(0,[x[::3,::3],y[::3,::3]])
+#plt.quiver(x[::3,::3],y[::3,::3],pu,pv)
+plt.title('$\\sigma$, integration time = -1hr')
+
+plt.subplot(322)
+plt.contourf(x,y,sigma,levels=np.linspace(sigma.min(axis=None),sigma.max(axis=None),301))
+plt.colorbar()
+#pu,pv = vel_func(0,[x[::3,::3],y[::3,::3]])
+#plt.quiver(x[::3,::3],y[::3,::3],pu,pv)
+plt.title('$\\sigma$, integration time  t = -5days')
+
+#plt.gca().set_aspect('equal', adjustable='box', anchor='C')
+
+u,v = vel_func(0,[x,y])
+
+dudy,dudx = np.gradient(u,dy,dx)
+dvdy,dvdx = np.gradient(v,dy,dx)
+
+
+s1 = np.ma.empty([dimy,dimx])
+rhodot = np.ma.empty([dimy,dimx])
+nudot = np.ma.empty([dimy,dimx])
+J = np.array([[0, 1], [-1, 0]])
+for i in range(dimy):
+    for j in range(dimx):
+        if (dudx[i,j] and dudy[i,j] and dvdx[i,j] and dvdy[i,j] and u[i,j] and v[i,j]) is not np.ma.masked:    
+            Utemp = np.array([u[i, j], v[i, j]])
+            if np.dot(Utemp, Utemp) != 0:
+                Grad = np.array([[dudx[i, j], dudy[i, j]], [dvdx[i, j], dvdy[i, j]]])
+                S = 0.5*(Grad + np.transpose(Grad))
+                s1[i,j] = 3600*np.min(np.linalg.eig(S)[0])
+                rhodot[i, j] = 3600*np.dot(Utemp, np.dot(np.dot(np.transpose(J), np.dot(S, J)), Utemp))/np.dot(Utemp, Utemp)
+                nudot[i, j] = 3600*np.dot(Utemp,np.dot(np.trace(S)*np.identity(2) - 2*S, Utemp))/np.dot(Utemp, Utemp)
+            else:
+                rhodot[i,j] = np.ma.masked
+                nudot[i,j] = np.ma.masked
+                s1[i,j] = np.ma.masked
+
+        else:
+            rhodot[i,j] = np.ma.masked
+            nudot[i,j] = np.ma.masked
+            s1[i,j] = np.ma.masked
+plt.register_cmap(name='co', data=co())
+rholevel = np.min(np.abs([rhodot.min(axis=None),rhodot.max(axis=None)]))
+nulevel = np.min(np.abs([nudot.min(axis=None),nudot.max(axis=None)]))
+plt.subplot(323)
+plt.contourf(x,y,rhodot,levels=np.linspace(rhodot.min(axis=None),rhodot.max(axis=None),301),\
+             vmin=-2/3*rholevel,vmax=2/3*rholevel,cmap='co')
+plt.colorbar()#format=ticker.FuncFormatter(mf.fmt))
+plt.title('$\dot{\\rho}$')
+
+plt.subplot(324)
+plt.contourf(x,y,nudot,levels=np.linspace(nudot.min(axis=None),nudot.max(axis=None),301),\
+             vmin=-2/3*nulevel,vmax=2/3*nulevel,cmap='PiYG')
+plt.colorbar()
+plt.title('$\dot{\\nu}$')
+
+rdim = rhodot.shape
+for i in range(rdim[0]):
+    for j in range(rdim[1]):
+        if (rhodot[i,j]>0 and nudot[i,j]>0) or (rhodot[i,j]<0 and nudot[i,j]<0):
+            'nothing'
+            #rhodot = rhodot[(rhodot>0 and nudot>0) or (rhodot<0 and nudot<0)]
+        else:
+            rhodot[i,j] = np.ma.masked
+
+plt.subplot(325)
+plt.contourf(x,y,rhodot,levels=np.linspace(rhodot.min(axis=None),rhodot.max(axis=None),301),\
+             vmin=-2/3*rholevel,vmax=2/3*rholevel,cmap='co')
+plt.colorbar()#format=ticker.FuncFormatter(mf.fmt))
+plt.title('$\dot{\\rho}$ filtered by $\dot{\\nu}$')
+
+
+plt.subplot(326)
+plt.contourf(x,y,s1,levels=np.linspace(s1.min(axis=None),s1.max(axis=None),301))
+plt.colorbar()
+plt.title('s$_{-1}$')
+
+fig=plt.figure(4)
+plt.plot(sigma2.ravel())
---
+++ velocities.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 28 12:52:04 2018
+
+@author: pnola
+"""
+import numpy as np
+
+def co():
+    cdict = {'red':  [(0.0, 0.0000, 0.0000),
+                  (0.5, 1.0000, 1.0000),
+                  (1.0, 1.0000, 1.0000)],
+        'green': [(0.0, 0.5450, 0.5450),
+                  (0.5, 1.0000, 1.0000),
+                  (1.0, 0.5450, 0.5450)],
+        'blue':  [(0.0, 0.5450, 0.5450),
+                  (0.5, 1.0000, 1.0000),
+                  (1.0, 0.0000, 0.0000)]}
+    return cdict
+
+def double_gyre(t,Y,A = 0.1,w = np.pi*0.2,e = 0.25):
+    a = e*np.sin(w*t)
+    b = 1-2*e*np.sin(w*t)
+    f = a*Y[0]**2+b*Y[0]
+    dfdx = 2*a*Y[0]+b    
+    u =-np.pi*A*np.sin(np.pi*f)*np.cos(Y[1]*np.pi)    
+    v = np.pi*A*np.cos(np.pi*f)*np.sin(Y[1]*np.pi)*dfdx
+    return [u,v]
+
+def bickley_jet(t,Y):
+    #Scaled Down 4000
+    U0=62.66 #m/s
+    U0 = 3.6*U0 #km/hr
+    U0 = U0/4000*24 #1/day
+    L = 1770.0 #kms
+    L = L/4000 # no dim
+    A2 = 0.1
+    A3 = 0.3
+    c2 = 0.205*U0
+    c3 = 0.461*U0
+    re = 6371.0 #earths radius, kms
+    re = re/4000 #no dim
+    #kn = n*2/re
+    k2 = 4/re
+    k3 = 6/re
+    u = U0*((1/np.cosh(Y[1]/L))**2) + 2*A3*U0*np.tanh(Y[1]/L)*((1/np.cosh(Y[1]/L))**2)*np.cos(k3*(Y[0]-c3*t)) + 2*A2*U0*np.tanh(Y[1]/L)*((1/np.cosh(Y[1]/L))**2)*np.cos(k2*(Y[0]-c2*t))
+    v = -k3*A3*L*U0*((1/np.cosh(Y[1]/L))**2)*np.sin(k3*(Y[0]-c3*t)) - k2*A2*L*U0*((1/np.cosh(Y[1]/L))**2)*np.sin(k2*(Y[0]-c2*t))
+    return [u,v]
+
+def cell(t,Y):
+    u =-0.1*np.cos(np.pi*Y[0]-np.pi/2.0)*np.sin(np.pi*Y[1]-np.pi/2.0)
+    v = 0.1*np.sin(np.pi*Y[0]-np.pi/2.0)*np.cos(np.pi*Y[1]-np.pi/2.0)    
+    return [u,v]
+
+
+def glider(t,Y):
+    u = np.sqrt(Y[0]**2 + Y[1]**2)*( -Y[1]*( 1.2*np.sin(-2*np.arctan2(Y[1],Y[0]))) - (1.4-np.cos(-2*np.arctan2(Y[1],Y[0])))*Y[0])
+    v = np.sqrt(Y[0]**2 + Y[1]**2)*( Y[0]*( 1.2*np.sin(-2*np.arctan2(Y[1],Y[0])) ) - (1.4-np.cos(-2*np.arctan2(Y[1],Y[0])))*Y[1]) - 1
+    return [u,v]
+
+	
+def hills_vortex(t,Y):
+    u = 2.0*Y[0]*Y[1];
+    v = -2.0*(2.0*Y[0]*Y[0]-1.0) - 2.0*Y[1]*Y[1]
+    return [u,v]
+  
+def sigmoidal(t,Y):
+    u = 0.4*np.tanh(10.0*(Y[0]-0.5*t))
+    v = 0.0
+    return [u,v]
+  
+def duffing_oscillator(t,Y):
+    u = Y[1]
+    v = Y[0] - Y[0]*Y[0]*Y[0]
+    return [u,v]
+  
+def campagnola(t,Y,pendulum_amplitude=0.5):
+    u = 0.0;
+    if(Y[0]<pendulum_amplitude*t+5): 
+        v = 1.0
+    else:
+        v = -1.0
+    return [u,v]
+
+def pendulum(t,Y,pendulum_amplitude=0.5):
+    u = Y[1]
+    v = -np.sin(Y[0])-pendulum_amplitude*Y[1]*np.sin(np.pi*t)
+    return [u,v]
+
+  
+def hurricane(t,Y,eye_alpha=0.2,eye_omega=0.8,eye_amplitude=0.4):
+    sqrtterm = np.sqrt(1.0+4.0*eye_alpha)
+    beta = 1.0/sqrtterm
+    yterm = Y[1]-0.5*(1.0+sqrtterm)
+  
+    u = -yterm/(Y[0]*Y[0]+yterm*yterm+eye_alpha) - beta
+    v = Y[0]/(Y[0]*Y[0]+yterm*yterm+eye_alpha) + eye_amplitude*Y[1]*np.cos(eye_omega*t)
+    return [u,v]
+  
+def wedge(t,Y,W_lambda=0.5,W_alpha=0.5,W_beta=0.5,W_K0=0.5):
+  
+    c1 = np.cos(W_lambda*Y[1])
+    s1 = np.sin(W_lambda*Y[1])
+  
+    c2 = np.cos((W_lambda-2.0)*Y[1])
+    s2 = np.sin((W_lambda-2.0)*Y[1])
+  
+    psi_theta = np.real(pow(Y[0],W_lambda)*W_K0*(-W_alpha*W_lambda*s1 + (W_lambda-2.0)*W_beta*s2))
+    psi_r = np.real(W_lambda*pow(Y[0],W_lambda-1.0)*W_K0*(W_alpha*c1 - W_beta*c2))
+  
+    u = 2.0*np.pi*psi_theta/Y[0]
+    v = -2.0*np.pi*psi_r/Y[0]
+    return [u,v]
+
+def weight(t,Y):
+    weight = 0.1+np.exp(-(Y[0]*Y[0]+Y[1]*Y[1])/4.0)
+    u = (-2*Y[1]-Y[1]*Y[1])*weight;
+    v = (2*Y[0]-Y[0]*Y[1])*weight
+    return [u,v]
+
+'''
+x = np.linspace(-2,2,11)
+y = np.linspace(-1,1,11)
+x,y=np.meshgrid(x,y)
+u,v=glider(0,[x,y])
+import matplotlib.pyplot as plt
+plt.close('all')
+plt.quiver(x,y,u,v)
+
+'''
