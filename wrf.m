e25cafbe757369e125a58fedbe3aad5a26001f6f
Peter <pnolan1986@gmail.com>
2019-01-17 17:32:39 -0500

    updates

--- wrf.m
+++ wrf.m
@@ -1,8 +1,8 @@
 %
 close all
-%clear all
+clear all
 clc
-%
+
 dt=1 %hr
 dx=12 %kms
 dy=12 %kms
@@ -11,32 +11,6 @@ dt=3600 %s
 dx=12*1000 %m
 dy=12*1000 %m
 %}
-ncfile='ftle_80m.nc';
-ncid=netcdf.open(ncfile,'NC_NOWRITE');
-
-%{
-Grab NC data
-[ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
-%
-%Loop through NC data infomation
-for i = 1:nvars
-    i-1
-    [varname,xtype,dimids,natts] = netcdf.inqVar(ncid,i-1)
-end
-%}
-
-time_ftle = netcdf.getVar(ncid,0); %days
-%time_ftle=24*time_ftle(1:6:end); %hrs
-time_ftle=24*time_ftle; %hrs
-lon = netcdf.getVar(ncid,1,'double');
-lat = netcdf.getVar(ncid,2,'double');
-ftle = netcdf.getVar(ncid,5); %days^{-1}
-%ftle=permute(squeeze(ftle(1,:,:,1:6:end)),[2,1,3]);
-ftle=permute(squeeze(ftle(1,:,:,:)),[2,1,3]);
-ftle(ftle==999999)=nan;
-ftle=1/24*ftle; %hrs^{-1}
-[ydim,xdim,tdim]=size(ftle)
-%
 ncfile='hosiendata_wind_velocity.nc';
 ncid=netcdf.open(ncfile,'NC_NOWRITE');
 time_U = netcdf.getVar(ncid,0);
@@ -48,7 +22,7 @@ v = netcdf.getVar(ncid,4); %m/s
 v = permute(v,[2,1,3]);
 v(v==999)=nan;
 v=3.6*v; %km/hr
-%[ydim,xdim,tdim]=size(u);
+[ydim,xdim,tdim]=size(u);
 %
 [dudx,dudy,dudt] = gradient(u,dx,dy,dt);
 [dvdx,dvdy,dvdt] = gradient(v,dx,dy,dt);
@@ -84,76 +58,121 @@ for t =1:length(time_U)
         end
     end
 end
-%ftle(s1_numerical==nan)=nan;
-%ftle(cor_numerical==nan)=nan;
-%}
-figure
-subplot(121)
-surface(-s1_numerical(:,:,end),'edgecolor','none')
-title('numerical s1')
-colorbar()
-subplot(122)
-surface(cor_numerical(:,:,end),'edgecolor','none')
-title('numerical correction')
-colorbar()
-%
-%{
-figure
-surface(x,y,-s1_numerical(:,:,3)+1*cor_numerical(:,:,3),'edgecolor','none')
-title('numerical')
-colorbar()
-%}
-%}
-s1 = s1_numerical(:,:,end);
-c1 = cor_numerical(:,:,end);
-ftle(:,:,end)=-s1;
-time = time_ftle;
-n = length(time);
-for i =1:n
-    T=-time(i);
-    ftle_t = squeeze(ftle(:,:,end+1-i));
-    sig_true = reshape(ftle_t,[],1);
-    sig_approx = reshape(-s1-T*c1,[],1);
-    ind = ~isnan(sig_true) & ~isnan(sig_approx) ;
-    sig_true = sig_true(ind);
-    sig_approx = sig_approx(ind);
-    rmse_corrected(i) = sqrt(mean((sig_approx-sig_true).^2));
-    sa_bar = mean(sig_approx);
-    st_bar = mean(sig_true);
-    numerator = sum(sig_approx.*sig_true)-(n*sa_bar*st_bar);
-    den1 = sqrt(sum(sig_approx.^2)-n*sa_bar.^2);
-    den2 = sqrt(sum(sig_true.^2)-n*st_bar.^2);
-    denominator = den1*den2;
-    cor_corrected(i) = numerator./denominator;
-    
-    sig_approx = reshape(-s1,[],1);
-    sig_approx = sig_approx(ind);
-    rmse_uncorrected(i) = sqrt(mean((sig_approx-sig_true).^2));
-    sa_bar = mean(sig_approx);
-    st_bar = mean(sig_true);
-    numerator = sum(sig_approx.*sig_true)-(n*sa_bar*st_bar);
-    den1 = sqrt(sum(sig_approx.^2)-n*sa_bar.^2);
-    den2 = sqrt(sum(sig_true.^2)-n*st_bar.^2);
-    denominator = den1*den2;
-    cor_uncorrected(i) = numerator./denominator;
-    
-    %cor(i) = corr(sig_approx,sig_true);
-end
 
-figure
-subplot(121)
-hold on
-plot(time,rmse_corrected,'b')
-plot(time,rmse_uncorrected,'r')
-legend('-s1-T*corr','-s1')
-ylabel('rmse')
-xlabel('|T|')
-subplot(122)
-hold on
-plot(time,cor_corrected,'b')
-plot(time,cor_uncorrected,'r')
-ylabel('correlation')
-xlabel('|T|')
-legend('-s1-T*corr','-s1')
+
+for Q = 3:0.1:4
+    %
+    
+    ncfile='ftle_80m.nc';
+    ncid=netcdf.open(ncfile,'NC_NOWRITE');
+
+    %{
+    Grab NC data
+    [ndims,nvars,ngatts,unlimdimid] = netcdf.inq(ncid);
+    %
+    %Loop through NC data infomation
+    for i = 1:nvars
+        i-1
+        [varname,xtype,dimids,natts] = netcdf.inqVar(ncid,i-1)
+    end
+    %}
+
+    time_ftle = netcdf.getVar(ncid,0); %days
+    %time_ftle=24*time_ftle(1:6:end); %hrs
+    time_ftle=24*time_ftle; %hrs
+    lon = netcdf.getVar(ncid,1,'double');
+    lat = netcdf.getVar(ncid,2,'double');
+    ftle = netcdf.getVar(ncid,5); %days^{-1}
+    %ftle=permute(squeeze(ftle(1,:,:,1:6:end)),[2,1,3]);
+    ftle=permute(squeeze(ftle(1,:,:,:)),[2,1,3]);
+    ftle(ftle==999999)=nan;
+    ftle=1/(24*Q)*ftle; %hrs^{-1}
+    [ydim,xdim,tdim]=size(ftle)
+    %
+    %ftle(s1_numerical==nan)=nan;
+    %ftle(cor_numerical==nan)=nan;
+    %}
+    figure
+    subplot(121)
+    surface(-s1_numerical(:,:,end),'edgecolor','none')
+    title('numerical s1')
+    colorbar()
+    subplot(122)
+    surface(cor_numerical(:,:,end),'edgecolor','none')
+    title('numerical correction')
+    colorbar()
+    %
+    %{
+    figure
+    surface(x,y,-s1_numerical(:,:,3)+1*cor_numerical(:,:,3),'edgecolor','none')
+    title('numerical')
+    colorbar()
+    %}
+    %}
+    s1 = s1_numerical(:,:,end);
+    c1 = cor_numerical(:,:,end);
+    ftle(:,:,end)=-s1;
+    time = time_ftle;
+    n = length(time);
+    for i =1:n
+        T=-time(i);
+        ftle_t = squeeze(ftle(:,:,end+1-i));
+        sig_true = reshape(ftle_t,[],1);
+        sig_approx = reshape(-s1-T*c1,[],1);
+        ind = ~isnan(sig_true) & ~isnan(sig_approx) ;
+        sig_true = sig_true(ind);
+        sig_approx = sig_approx(ind);
+        rmse_corrected(i) = sqrt(mean((sig_approx-sig_true).^2));
+        sa_bar = mean(sig_approx);
+        st_bar = mean(sig_true);
+        n=length(sig_true);
+        numerator = sum(sig_approx.*sig_true)-(n*sa_bar*st_bar);
+        den1 = sqrt(sum(sig_approx.^2)-n*sa_bar.^2);
+        den2 = sqrt(sum(sig_true.^2)-n*st_bar.^2);
+        denominator = den1*den2;
+        cor_corrected(i) = numerator./denominator;
+
+        sig_approx = reshape(-s1,[],1);
+        sig_approx = sig_approx(ind);
+        rmse_uncorrected(i) = sqrt(mean((sig_approx-sig_true).^2));
+        sa_bar = mean(sig_approx);
+        st_bar = mean(sig_true);
+        numerator = sum(sig_approx.*sig_true)-(n*sa_bar*st_bar);
+        den1 = sqrt(sum(sig_approx.^2)-n*sa_bar.^2);
+        den2 = sqrt(sum(sig_true.^2)-n*st_bar.^2);
+        denominator = den1*den2;
+        cor_uncorrected(i) = numerator./denominator;
+
+        %cor(i) = corr(sig_approx,sig_true);
+    end
+    %time=time(2:27)
+    start = 2;
+    stop = 16;
+
+    time = time*60;
+    time=time(start:stop);
+    rmse_corrected=rmse_corrected(start:stop)
+    rmse_uncorrected=rmse_uncorrected(start:stop)
+    cor_corrected=cor_corrected(start:stop)
+    cor_uncorrected=cor_uncorrected(start:stop)
+    size(time)
+    size(rmse_corrected)
+    fig = figure
+    subplot(121)
+    hold on
+    plot(time,rmse_corrected,'b')
+    plot(time,rmse_uncorrected,'r')
+    legend('-s1-T*corr','-s1')
+    ylabel('rmse')
+    xlabel('|T|')
+    subplot(122)
+    hold on
+    plot(time,cor_corrected,'b')
+    plot(time,cor_uncorrected,'r')
+    ylabel('correlation')
+    xlabel('|T|')
+    legend('-s1-T*corr','-s1')
+    saveas(fig,sprintf('%1.1f.fig',Q))
+end
 
 %}
