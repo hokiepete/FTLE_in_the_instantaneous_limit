92432966ef19e6cb0a66d675a5f29dd1ec6b3435
Peter <pnolan1986@gmail.com>
2019-01-22 01:08:40 -0500

    updates

--- plot_rmse_4paper.py
+++ plot_rmse_4paper.py
@@ -22,11 +22,11 @@ s1 = f['rmse_uncorrected'][0]
 plt.figure(figsize=figSize)
 plt.plot(t,s1,'b-')
 plt.plot(t,cor,'m-')
-plt.ylabel('FTLE field root mean-squared error')
-plt.xlabel('$|T|$')
+plt.ylabel('FTLE field root mean-squared error',**labelfont)
+plt.xlabel('$|T|$',**labelfont)
 plt.axis('tight')
 plt.xlim([0,0.7])
-plt.savefig('dg_rmse.png', transparent=False, bbox_inches='tight',pad_inches=0.03)
+plt.savefig('dg_rmse.eps', transparent=False, bbox_inches='tight',pad_inches=0.03)
 
 
 f = sio.loadmat('wrf_plot_data.mat')
@@ -37,10 +37,10 @@ s1 = f['rmse_uncorrected'][0]
 plt.figure(figsize=figSize)
 plt.plot(t,s1,'b-')
 plt.plot(t,cor,'m-')
-plt.ylabel('FTLE field root mean-squared error')
-plt.xlabel('$|T|$')
+plt.ylabel('FTLE field root mean-squared error',**labelfont)
+plt.xlabel('$|T|$',**labelfont)
 plt.xlim([0,0.7])
-plt.savefig('wrf_rmse.png', transparent=False, bbox_inches='tight',pad_inches=0.03)
+plt.savefig('wrf_rmse.eps', transparent=False, bbox_inches='tight',pad_inches=0.03)
 
 
 f = sio.loadmat('dg3d_plot_data.mat')
@@ -51,10 +51,10 @@ s1 = f['rmse_uncorrected'][0]
 plt.figure(figsize=figSize)
 plt.plot(t,s1,'b-')
 plt.plot(t,cor,'m-')
-plt.ylabel('FTLE field root mean-squared error')
-plt.xlabel('$|T|$')
+plt.ylabel('FTLE field root mean-squared error',**labelfont)
+plt.xlabel('$|T|$',**labelfont)
 plt.xlim([0,0.7])
-plt.savefig('dg3d_rmse.png', transparent=False, bbox_inches='tight',pad_inches=0.03)
+plt.savefig('dg3d_rmse.eps', transparent=False, bbox_inches='tight',pad_inches=0.03)
 
 
 f = sio.loadmat('abc_plot_data.mat')
@@ -65,9 +65,9 @@ s1 = f['rmse_uncorrected'][0]
 plt.figure(figsize=figSize)
 plt.plot(t,s1,'b-')
 plt.plot(t,cor,'m-')
-plt.ylabel('FTLE field root mean-squared error')
-plt.xlabel('$|T|$')
+plt.ylabel('FTLE field root mean-squared error',**labelfont)
+plt.xlabel('$|T|$',**labelfont)
 plt.xlim([0,0.7])
-plt.savefig('abc_rmse.png', transparent=False, bbox_inches='tight',pad_inches=0.03)
+plt.savefig('abc_rmse.eps', transparent=False, bbox_inches='tight',pad_inches=0.03)
 
 del f
