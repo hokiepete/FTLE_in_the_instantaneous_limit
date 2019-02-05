18de667eef74cca60b18edee3d7131164ae5456d
Peter <pnolan1986@gmail.com>
2019-02-05 13:59:39 -0500

    updates

--- 2dDouble_gyre/dg_ftle.m
+++ 2dDouble_gyre/dg_ftle.m
@@ -1,49 +1,51 @@
 clear all
 close all
 clc
+
+%Set up the domain
 lenx = 31
-lenyz = 15
+leny = 15
 tlen = 101
 t0 = 0
 tf = -0.1
 x = linspace(0,2,lenx);
 dx=x(2)-x(1);
-y = linspace(0,1,lenyz);
+y = linspace(0,1,leny);
 dy=y(2)-y(1);
-twant = linspace(t0,tf,tlen);
+time = linspace(t0,tf,tlen);
 [x,y] = meshgrid(x,y);
+
+%Calculate the flow map
 options = odeset('RelTol',1e-12,'AbsTol',1e-12);
-for i = 1:lenyz
-    i
+for i = 1:leny
     for j = 1:lenx
             y0=[x(i,j),y(i,j)];
             [t,yout] = ode45(@dg_int,[t0,tf],y0,options);
-            fx(i,j,:) = interp1(t,yout(:,1),twant,'spline');
-            fy(i,j,:) = interp1(t,yout(:,2),twant,'spline');
+            fx(i,j,:) = interp1(t,yout(:,1),time,'spline');
+            fy(i,j,:) = interp1(t,yout(:,2),time,'spline');
     end
 end
-sigma = zeros([lenyz,lenx,tlen]);
-size(sigma)
+
+%Compute the FTLE
+sigma = zeros([leny,lenx,tlen]);
 for t = 1:tlen
-    
     %need to skip t0 to avoid division by zero
     %set FTLE to zero instead
-    if twant(t) == t0
+    if time(t) == t0
         continue
     else
         [dfxdx,dfxdy] = gradient(squeeze(fx(:,:,t)),dx,dy);
         [dfydx,dfydy] = gradient(squeeze(fy(:,:,t)),dx,dy);
-        for i = 1:lenyz
+        for i = 1:leny
             for j = 1:lenx
                     gradF = [dfxdx(i,j),dfxdy(i,j);
                              dfydx(i,j),dfydy(i,j)];
                     C = gradF'*gradF;
                     lambda = max(eig(C));
-                    sigma(i,j,t) = 1/(2*abs(twant(t)-t0))*log(lambda);
+                    sigma(i,j,t) = 1/(2*abs(time(t)-t0))*log(lambda);
             end
         end
     end
 end
-size(sigma)
-time=twant;
-save('dg_sigma_small_short.mat', 'sigma','time');
+
+save('dg_sigma.mat', 'sigma','time');
